e10259117b1250cab08252321be6c0bb86fbcda4
Vishal Srinivas <vishal1995srinivas@gmail.com>
2019-11-07 16:05:43 +0530

    Top economy bowlers in 2015

--- ipl.js
+++ ipl.js
@@ -104,6 +104,96 @@ CSVToJSON().fromFile("./matches.csv").then(matches => {
 });
   
  });
- /**************************Fourth query solution will be continued tomorrow ****************************************************** */
+ /**************************Fourth query solution ****************************************************** */
  
+CSVToJSON().fromFile("./matches.csv").then(matches => {
+   
+
+    let match_id_2015 = [];
+    for(count of matches)
+    {
+        if(count.season == '2015')
+        {
+            match_id_2015.push(count.id);
+        }
+    }
+   //Now we got match ids of 2015 by above for loop----------------------------------------------------
+   CSVToJSON().fromFile("./deliveries.csv").then(deliveries_2015 => {
+    let total_runs = {};
+   // console.log(deliveries[0]);/*
+    for(let i = 0;i < match_id_2015.length; i++ )
+    {
+      // console.log(match_id[i]);
+        for(match of deliveries_2015)
+        {
+           // console.log(match.bowling_team);
+            if(match_id_2015[i] == match.match_id)
+            {
+               
+                if(total_runs[match.bowler] == undefined)
+                {
+                    total_runs[match.bowler] = new Array();
+                    total_runs[match.bowler][0] = 0;
+                    total_runs[match.bowler][1] = 1;
+                }
+                if(total_runs.hasOwnProperty(match.bowler)==true)
+                {
+                   
+                   total_runs[match.bowler][0] += parseInt(match.total_runs); // no of runs
+                   total_runs[match.bowler][1] += 1; // no of deliveries
+
+                }
+            }
+        }
+
+    }
+    //Matched 2015 match-ids in deiveries.csv and we got 'bowler name': [total runs , total delveries]-------------------
+    let sort = [];
+    let economy = [];
+    //Getting economy rate and popping out balls!!!!!! ---------------------------------------------
+    for(key in total_runs)
+    {
+      total_runs[key][0] = ( total_runs[key][0] / total_runs[key][1] ) * 6;
+      total_runs[key].pop();
+      sort.push(total_runs[key][0]);
+    }
+    bubble_Sort(sort);
+    //to find most economical bowler - bubble sort-----------------------------------
+    function swap(arr, first_Index, second_Index){
+        var temp = arr[first_Index];
+        arr[first_Index] = arr[second_Index];
+        arr[second_Index] = temp;
+    }
+    function bubble_Sort(arr){
+    
+        var len = arr.length,
+            i, j, stop;
+    
+        for (i=0; i < len; i++){
+            for (j=0, stop=len-i; j < stop; j++){
+                if (arr[j] > arr[j+1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    
+        sort = arr;
+    }
+   //Matching bowler(key) with economy rate------------------------------------------------------------------
+    for(let i=0;i<sort.length;i++)
+    {
+        for(key in total_runs)
+        {
+           if(sort[i] == total_runs[key][0])
+           {
+               economy[key] = new Array();
+               economy[key].push(sort[i]);
+           } 
+       }
+    }
+    console.log("Top economical bowlers of 2015 with their economy rate are as follows \n",economy);
+//----------------------------Finally done-------------------------------------------------------------------
+});
+ });
+/***********************5th query on process***************************************************************/
 
