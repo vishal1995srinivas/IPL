053cc5500e666e423278190883963b8fadae127e
Vishal Srinivas <vishal1995srinivas@gmail.com>
2019-11-18 17:37:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const CSVToJSON = require("csvtojson");
 const FileSystem = require("fs");
-
 CSVToJSON().fromFile("./matches.csv").then(matches => {
     const no_of_wins = require('./second_query');
     const no_of_matches_per_yr = require('./first_query');
@@ -13,6 +12,5 @@ CSVToJSON().fromFile("./matches.csv").then(matches => {
         extra_runs_scored_by_all_teams(matches, deliveries);
         top_economical_bowlers_2016(matches, deliveries);
         top_maiden_overs_bowled_bowlers(deliveries);
-
     });
 });
